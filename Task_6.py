2af26221b4a7a50987e32dfdaeab21c36e9b397b
D <libra.07d@gmail.com>
2026-06-17 10:21:37 +0700

    Create Task_6.py

---
+++ Task_6.py
@@ -0,0 +1,12 @@
+def func(x = 0,  y = 0):
+    return x**2 + y**2
+    
+x = 1 + 2 * 3 - 4 / 5
+
+list1 = [1, 2, 3, 4, 5]
+dict1 = {'a': 1, 'b': 2, 'c': 3}
+
+result = func(2,3)
+
+if result > 10 and result < 20:
+    print('Result in range')
