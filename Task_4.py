8a045fe216239c1424019a936da5d4e85ca3ff33
D <libra.07d@gmail.com>
2026-06-17 10:17:07 +0700

    Create Task_4.py

---
+++ Task_4.py
@@ -0,0 +1,19 @@
+def func1():
+    x = 1
+    y = 2
+    return x + y
+
+
+def func2(a,b):
+    if a > b:
+        return a
+    else:
+        return b
+
+
+class Test:
+    def mathod1(self):
+        pass
+
+    def mathod2(self):
+        pass
